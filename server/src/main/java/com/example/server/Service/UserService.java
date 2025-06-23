35a53775f7a4ec7d3cf5ad8d798faf1a0fd012e4
Yujinkami <kohtet112@gmail.com>
2025-06-24 00:04:24 +0630

    add my pj

---
+++ server/src/main/java/com/example/server/Service/UserService.java
@@ -0,0 +1,18 @@
+package com.example.server.Service;
+
+import com.example.server.Model.User;
+import com.example.server.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UserService {
+    
+    @Autowired
+    private UserRepository userRepository;
+    
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
